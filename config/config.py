984d84f64b04dce173b8587e9fff3339238b174e
Kassim752 <100793480+Kassim752@users.noreply.github.com>
2023-03-16 11:30:15 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kassim-the-devil/LoveHurtsMusic",
+    "https://github.com/kassim752/ShizukaXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
